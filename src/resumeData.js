a2e873ec179653ee45aeeb4657d00a712a58c086
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2023-02-12 22:19:06 -0800

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,91 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Marcus Hudnell",
+    "role": "Full-Stack Software Developer",
+    "linkedinId":"",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I am full-stack software engineer that prefers to work in the backend. I've been a working professional for 10+ years and in my free time I love to make music. I am constantly amazed at the creative and critical thinking overlap between creating music and software development. I aspire to write clean, maintainable and scalable code and hope to be an asset to your team!",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/marcus-hudnell/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/hudnellmarcus",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am full-stack software developer that prefers to work in the backend. I've been a working professional for 10+ years and in my free time I love to make music. I am constantly amazed at the creative and critical thinking overlap between creating music and software development. I'm not afraid of any challenge as I truly believe that diamonds are made under the greatest pressure.",
+    "address":"Los Angeles, CA",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"The LNM Insitute of Information Technology",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Oddsmakers",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"TeeTime",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"Festivalist",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"The Spirit Within",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+  }
+  
+  export default resumeData
