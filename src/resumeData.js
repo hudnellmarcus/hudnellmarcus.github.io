630b8c121a892b559bc2ef2a29ac13ef40153759
Marcus Hudnell <103913070+hudnellmarcus@users.noreply.github.com>
2023-07-09 19:40:16 -0700

    first commit of new repo

---
+++ src/resumeData.js
@@ -0,0 +1,104 @@
+let resumeData = {
+    "imagebaseurl":"https://hudnellmarcus.github.io/",
+    "name": "Marcus Hudnell",
+    "role": "Full-Stack Software Developer",
+    "linkedinId":"",
+    "skypeid": "Your skypeid",
+    "roleDescription": "As a driven and detail-oriented software engineer with experience in both front-end and back-end technologies, I am passionate about creating innovative solutions that help businesses thrive. With a solid foundation in programming fundamentals and a natural ability to problem-solve, I strive to stay up-to-date on the latest industry trends and technologies to help clients achieve their goals. I am a collaborative team player with excellent communication skills, and I approach each project with a strong sense of ownership and a dedication to quality.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/marcus-hudnell/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/hudnellmarcus",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme": "Beyond professional work, I am  also a musician who loves to explore new sounds and create original music in my spare time. My passion for music inspires my work in technology, encouraging me to think creatively and seek innovative solutions to every problem. I am constantly amazed at the creative and critical thinking overlap between creating music and software development. I'm not afraid of any challenge as I truly believe that diamonds are made under the greatest pressure.",
+    "address":"Los Angeles, CA",
+    "website":"hudnell.marcus@gmail.com",
+    "phone":"561.389.6421",
+    "education":[
+      {
+        "UniversityName":"Colgate University",
+        "specialization":"Liberal Arts",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2011",
+      },
+      {
+        "UniversityName":"General Assembly",
+        "specialization":"Software Engineering Certificate",
+        "MonthOfPassing":"Jul",
+        "YearOfPassing":"2022",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"images/icons/icons8-javascript-50.png"
+      },
+      {
+        "skillname":"images/icons/icons8-react-100.png"
+      },
+      {
+        "skillname":"images/icons/icons8-c++-50.png"
+      },
+      {
+        "skillname":"images/icons/icons8-firebase-96.png"
+      }
+    ],
+    "portfolio":[
+
+      {
+        "name":"Spotify Clone",
+        "description":"webapp",  
+        "imgurl":"images/spotify.png",
+        "url": "https://hudnellmarcus.github.io/spotify_clone/",
+        "github": "https://github.com/hudnellmarcus/spotify_clone"
+      },
+
+      {
+        "name":"Step-Sequencer",
+        "description":"webapp",
+        "imgurl":"images/step_sequencer.png",
+       "url": "https://hudnellmarcus.github.io/Step-Sequencer-Demo/",
+       "github": "https://github.com/hudnellmarcus/Step-Sequencer-Demo"
+      },
+
+      {
+        "name":"Oddsmakers",
+        "description":"webapp",
+        "imgurl":"images/oddsmakers.png",
+        "url": "https://hudnellmarcus.github.io/OddsMakers-/",
+        "github": "https://github.com/hudnellmarcus/OddsMakers-"
+      },
+      {
+        "name":"TeeTime",
+        "description":"webapp",
+        "imgurl":"images/TeeTime.png",
+        "github": "https://github.com/hudnellmarcus/TeeTime-front"
+      },
+    
+    ],
+  }
+  
+  export default resumeData
